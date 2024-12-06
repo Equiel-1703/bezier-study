9f07a4b2e0679900030395d5d8c0373924f59537
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-12-05 23:55:07 -0300

    feat: Improved overall appearence and  implemented QuadTree

--- scripts/bezier.js
+++ scripts/bezier.js
@@ -13,30 +13,41 @@ const pointToLocal = (point, localRect) => {
 }
 
 const canvas = $('#draw-canvas').get(0);
-const canvasGlobalRect = canvas.getBoundingClientRect();
+let canvasGlobalRect = canvas.getBoundingClientRect();
 const canvasLocalRect = new DOMRect(0, 0, canvasGlobalRect.width, canvasGlobalRect.height);
 const ctx = canvas.getContext('2d');
 
 const mouse = new CGUtils.Mouse();
 
-const quadTree = new CGUtils.QuadTree(canvasLocalRect, 4);
+export const quadTree = new CGUtils.QuadTree(canvasLocalRect, canvasGlobalRect, 4);
+
+// Update canvasGlobalRec every resize
+window.addEventListener('resize', (_e) => {
+	const newGlobalRect = canvas.getBoundingClientRect();
+	
+	canvasGlobalRect.x = newGlobalRect.x;
+	canvasGlobalRect.y = newGlobalRect.y;
+	canvasGlobalRect.width = newGlobalRect.width;
+	canvasGlobalRect.height = newGlobalRect.height;
+
+	quadTree.updateAllDivs();
+});
 
 // Handle user clicks
 canvas.addEventListener('click', (_e) => {
 	let point = pointToLocal(mouse.coords, canvasGlobalRect);
+	
 	drawPoint(point, ctx);
 
-	console.log(point);
-	
 	if (quadTree.addPoint(point)) {
-		console.log("point added to quadTree");
+		console.log("Point successfully added to quadTree.");
 	} else {
-		console.log("point not added!");
+		console.log("Point not added!");
 	}
 });
 
 $('body').on("keydown", (e) => {
-	if (!e.originalEvent.repeat && e.originalEvent.key == 'p') {
+	if (!e.originalEvent.repeat && e.originalEvent.key == 'q') {
 		console.log(quadTree);
 	}
 });
--- scripts/cg_utils.js
+++ scripts/cg_utils.js
@@ -1,4 +1,3 @@
-
 export class Mouse {
 	#mouseCoords = {x: 0, y: 0};
 	
@@ -34,15 +33,32 @@ export class Point {
 }
 
 export class QuadTree {
-	constructor(rect, capacity) {
-		this.rect = rect; // The rectangle that represents the bounds of the current node
+	constructor(rect, globalRec, capacity, level = 0) {
+		this.rect = rect; // The local rectangle that represents the bounds of the current node
 		this.nodeCapacity = capacity; // Max of points per node
 		this.points = []; // The points inside the node
 		this.childs = []; // Childs of this node (subdivisions)
+		
+		this.globalRec = globalRec; // The global rectangle of the canvas
+		this.level = level; // The level of this node in the tree
+		this.outlineDiv = this.#createOutlineDiv(); // The div that contains the outline of the node bounds
+
+		this.#updateDiv();
 	}
+
+	// Colors for the divs per level
+	#divColors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink', 'brown', 'gray', 'cyan'];
 
 	#haveChilds() {
 		return this.childs.length > 0;
+	}
+
+	#addPointToChilds(point) {
+		for (const c of this.childs) {
+			if (point.isInsideRectangle(c.rect)) {
+				return c.addPoint(point);
+			}
+		}
 	}
 
 	// Insert a point into the quadtree
@@ -50,21 +66,16 @@ export class QuadTree {
 		// First, we need to check if this node have childs (subdivisions)
 		if (this.#haveChilds()) {
 			// If yes, then we will add this point in the correct subdivision
-			for (const c of this.childs) {
-				if (point.isInsideRectangle(c.rect)) {
-					return c.addPoint(point);
-				}
-			}
+			return this.#addPointToChilds(point);
 		// If this node doesn't have a subdivision we will add the point here, if it's inside the bounds
 		} else if (point.isInsideRectangle(this.rect)) {
-			// Check node capacity
 			if (this.points.length < this.nodeCapacity) {
 				this.points.push(point);
 				return true;
 			} else {
-				// Subdivide if needed
-				console.log("Subdivided");
-				return this.#subdivide();
+				this.#subdivide();
+				console.log("QuadTree subdivided.");
+				return this.#addPointToChilds(point);
 			}
 		// If the process failed, the point is probably out of bounds
 		} else {
@@ -73,10 +84,70 @@ export class QuadTree {
 	}
 
 	#subdivide() {
-		// const halfWidth = this.rect.
+		const x = this.rect.x;
+		const y = this.rect.y;
+		const w = this.rect.width;
+		const h = this.rect.height;
+		const hw = this.rect.width / 2.0;
+		const hh = this.rect.height / 2.0;
+
+		// Print these values
+		console.log(`x: ${x}, y: ${y}, w: ${w}, h: ${h}, hw: ${hw}, hh: ${hh}`);
+		
+		// Create new divisions
+		const TL = new QuadTree(new DOMRect(x, y, hw, hh), this.globalRec, this.nodeCapacity, this.level + 1); // Top-Left
+		const TR = new QuadTree(new DOMRect(x + hw, y, hw, hh), this.globalRec, this.nodeCapacity, this.level + 1); // Top-Right
+		const BL = new QuadTree(new DOMRect(x, y + hh, hw, hh), this.globalRec, this.nodeCapacity, this.level + 1); // Bottom-Left
+		const BR = new QuadTree(new DOMRect(x + hw, y + hh, hw, hh), this.globalRec, this.nodeCapacity, this.level + 1); // Bottom-Right
+
+		// Add to childs array
+		this.childs.push(TL, TR, BL, BR);
+
+		// Move the points to the correct subdivisions
+		while (this.points.length > 0) {
+			let p = this.points.shift();
+			this.#addPointToChilds(p);
+		}
+	}
+
+	// Debug function to print the tree
+	print() {
+		console.log(this);
+		for (const c of this.childs) {
+			c.print();
+		}
+	}
+
+	// Debug function to create the outline div
+	#createOutlineDiv() {
+		const div = $(document.createElement('div'));
+
+		div.attr('class', 'quadtree-outline');
+		div.css('border-color', this.#divColors[this.level % this.#divColors.length]);
+		$('#quadtree-outline-container').append(div);
+
+		return div;
+	}
+
+	// Updates the outline div of the node to match the current position
+	#updateDiv() {
+		$(this.outlineDiv).css(
+			{
+				left: this.rect.x + this.globalRec.x,
+				top: this.rect.y + this.globalRec.y,
+				width: this.globalRec.width,
+				height: this.globalRec.height,
+			}
+		);
+	}
+
+	updateAllDivs() {
+		this.#updateDiv();
+		for (const c of this.childs) {
+			c.updateAllDivs();
+		}
 	}
 }
-
 
 export function lerp(from, to, amount) {
 	return from.mult(1 - amount).add(to.mult(amount));
